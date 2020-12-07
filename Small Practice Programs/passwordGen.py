487877e3da7804c45c0a5b91846b83a8f00bc5a6
Alexander Simak <59295630+asimak4@users.noreply.github.com>
2020-12-07 13:52:03 -0500

    Add files via upload

--- Small Practice Programs/passwordGen.py
+++ Small Practice Programs/passwordGen.py
@@ -1,5 +1,6 @@
 from random import choice
+
 password = ''
-password.join([choice('abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)') for i in range(20)])
+password = password.join([choice('abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)') for i in range(20)])
 
 print(password)
